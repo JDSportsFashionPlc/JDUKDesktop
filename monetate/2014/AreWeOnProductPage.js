98598372f278ccad761365f086f1d21b7f7dca01
Khaleel ibn Anwar Mughal <khaleelm@gmail.com>
2014-12-16 15:47:04 +0000

    Update AreWeOnProductPage.js

--- monetate/2014/AreWeOnProductPage.js
+++ monetate/2014/AreWeOnProductPage.js
@@ -14,7 +14,6 @@
 // -- This is stable as this is always on the Commerce ProductDisplay.jsp
 if ( jQuery('#productAttributesWrapper form').length ) {
 
-
   // What is the Plu we want to chec?
   // -- Simply enter the PLUID here, Commerce DB2 is manufacturerPartNumberId
     if ( jQuery('input[name="manufacturerPartNumber"]').val()=="106992" ) { 
