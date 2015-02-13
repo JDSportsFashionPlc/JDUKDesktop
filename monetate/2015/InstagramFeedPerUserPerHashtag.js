bbd75aee027d3d57ef912f9aaf6ee7618b754320
Khaleel ibn Anwar Mughal <khaleel.mughal@jdplc.com>
2015-02-13 14:40:12 +0000

    Updated the Monetate Group folder
    Added Instagram and changed the date to 2015

---
+++ monetate/2015/InstagramFeedPerUserPerHashtag.js
@@ -0,0 +1,58 @@
+/***
+* --- --- --- --- ---
+* JD Sports Fashion plc
+* Monetate, Inc.
+* Khaleel Mughal
+* --- --- --- --- ---
+* #JDINSTAGRAMFEEDPERUSERPERHASHTAG
+* --- --- --- --- ---
+***/
+
+//
+// GET INSTAGRAM FEED 
+// Get feed from Instagram based on Hashtag and filter by user
+// Also spit it inside bxSlider (this is optional)
+// 
+
+var apiURL = 'https://api.instagram.com/v1/';
+var username = 'jdsportsofficial';
+var hashtag = 'crlifestyle';
+var clientId = '5a79ddf3fa4147ffbea3fc0e38b22014';
+var auth_token = ''; // not needed for most
+var instaHTML;
+var divId = '#instafeed';
+var limit = 25;
+
+jQuery.ajax({
+    type: "GET",
+    dataType: "jsonp",
+    cache: false,
+    url: apiURL + hashtag + "/media/recent?client_id=" + clientId,
+    success: function (x) {
+        
+		  for (var i = 0; i < x.data.length && i < limit; i++) {
+            
+				// GET THE PICTURE
+            // -- options are thumbnail and large - see object for more
+            var instaPicture = x.data[i].images.thumbnail.url;
+            if (x.data[i].user.username == username) {
+                instaHTML += "<li class='CaroselSlideItem'><img src='" + instaPicture + "'/></li>";
+            }
+				
+            // INSERT THE GALLERY
+            jQuery(divId).html(instaHTML);
+        }
+		  
+		  // BXSLIDER API
+		  // -- This small chunk fires to bxSlider		  
+		  var caroselSlider = jQuery('#instafeed').bxSlider({
+			slideWidth:250,
+			minSlides:2,
+			maxSlides:5,
+			moveSlides:5,
+			slideMargin:15,
+			touchEnabled:true
+		});
+    }
+});
+
